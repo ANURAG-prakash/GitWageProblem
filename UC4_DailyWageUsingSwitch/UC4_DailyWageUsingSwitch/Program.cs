4d0e07c3875cee778dbc8daa9a22bd9fb466c79c
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-04 18:17:34 +0530

    Anuarg Prakash : Has Added

---
+++ UC4_DailyWageUsingSwitch/UC4_DailyWageUsingSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UC4_DailyWageUsingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IS_FULL_TIME = 2;
+            const int IS_PART_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            //variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
+
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp wage : " + empWage);
+            Console.ReadKey();
+        }
+    }
+}
