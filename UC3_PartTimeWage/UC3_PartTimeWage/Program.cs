373f77b1205426ff9a765980de4bc3a03b3c6b9a
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-03-04 18:14:18 +0530

    Anurag Prakash : Has Added UC3

---
+++ UC3_PartTimeWage/UC3_PartTimeWage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UC3_PartTimeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 2;
+            int PART_TIME_WAGE = 1; 
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+                empHrs = 8;
+            else if (empCheck == PART_TIME_WAGE)
+                empHrs = 4;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
+            Console.ReadKey();
+        }
+    }
+}
